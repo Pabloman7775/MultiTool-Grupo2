3dc21a3ae89c5e7972bed2c7e4658a01f3c30c1d
Fernández Miguel <serfermig2@alu.edu.gva.es>
2025-12-16 09:10:08 +0100

    Primer commit: Crear métodos básicos

---
+++ adivina_el_numero.java
@@ -0,0 +1,22 @@
+public class adivina_el_numero {
+
+    public static void adivina_el_numero_submenu() {
+
+    }
+
+    public static void adivina_el_numero_explicación() {
+
+    }
+
+    public static void adivina_el_numero_jugar() {
+
+    }
+
+    public static void adivina_el_numero_intentos() {
+
+    }
+
+    public static void adivina_el_numero_salir() {
+
+    }
+}
