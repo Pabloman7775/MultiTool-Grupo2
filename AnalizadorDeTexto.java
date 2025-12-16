5805926952eff74e0866d46ebf6b886470b3e522
Pabloman7775 <plopez.iesmoli@gmail.com>
2025-12-16 16:10:19 +0100

    Add files via upload
    subida de 2 aplicaciones mas

---
+++ AnalizadorDeTexto.java
@@ -0,0 +1,207 @@
+import java.util.Scanner;
+
+public class AnalizadorDeTexto {
+
+    public static Scanner leer =  new Scanner(System.in);
+    public static void main(String[] args) {
+
+        iniciarAnalizadorDeTexto();
+
+    }
+
+
+    
+// Inicio de aplicación (Main)
+
+    public static void iniciarAnalizadorDeTexto() {
+        int eleccion;
+
+        do {
+            eleccion = mostrarMenuYLeerOpcion();
+            
+            switch (eleccion) {
+                case 1:
+                    contarVocalesUI();
+                    break;
+
+                case 2:
+                    contarPalabrasUI();
+                    break;
+
+                case 3:
+                    palabraMasLargaUI();
+                    break;
+
+                case 0:
+                    System.out.println("\nHas salido del programa.");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+            System.out.println("\nPulsa ENTER para continuar...");
+            leer.nextLine();
+
+        } while (eleccion != 0);
+    }
+
+
+// Menú y validación de entradas
+
+    public static int mostrarMenuYLeerOpcion() {
+
+        boolean entradaValida = false;
+
+        String opcion;
+
+        do {
+
+            System.out.println("\n=== ANALIZADOR DE TEXTO ===");
+            System.out.println("1. Contar vocales");
+            System.out.println("2. Contar palabras");
+            System.out.println("3. Palabra más larga");
+            System.out.println("0. Salir del programa");
+            System.out.print("\nElige una opción: ");
+
+            opcion = leer.nextLine();
+            if (opcion.equals("0") || opcion.equals("1") || opcion.equals("2")|| opcion.equals("3")) {
+                entradaValida = true;
+            } else {
+                System.out.println("\nEntrada no válida. Introduce un valor compatible:");
+                
+                System.out.println("\nPulsa ENTER para continuar...");
+                leer.nextLine();
+
+                entradaValida = false;
+            }
+        } while (entradaValida == false);
+
+        // Conversión de String a int
+        int opcionInt = Integer.parseInt(opcion);
+
+        return opcionInt;
+    }
+
+
+    public static String leerYComprobarTexto() {
+
+        String texto = "";
+
+        boolean textoValido = false;
+        
+        do {
+            System.out.print("Introduce un texto: ");
+            texto = leer.nextLine();
+
+            if (!(texto == null || texto.isEmpty())) {
+                textoValido = true;
+                
+            } else if (texto == null || texto.isEmpty()) {
+                System.out.println("El texto introducido es incorrecto.");
+
+                System.out.println("\nPulsa ENTER para continuar...");
+                leer.nextLine();
+                textoValido = false;
+            }
+
+        } while (textoValido == false);
+
+
+        return texto;
+    }
+
+
+// Métodos interfaz del usuario
+
+    public static void palabraMasLargaUI() {
+        System.out.println("\n=== PALABRA MAS LARGA ===");
+        System.out.println("La palabra más larga del texto es '" + palabraMasLarga(leerYComprobarTexto()) + "'.");
+    }
+
+
+    public static void contarVocalesUI() {
+        System.out.println("\n=== CONTAR VOCALES ===");
+        System.out.println("El texto introducido contiene un total de " + contarVocales(leerYComprobarTexto()) + " vocales.");
+    }
+
+
+    public static void contarPalabrasUI() {
+        System.out.println("\n=== CONTAR PALABAS ===");
+        System.out.println("El texto contiene un total de " + contarPalabras(leerYComprobarTexto()) + " palabras.");
+    }
+
+
+// Métodos logicos
+
+    public static int contarVocales(String texto) {
+
+        texto = texto.toLowerCase();
+        int contadorVocales = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) == 'a' || texto.charAt(i) == 'e' || texto.charAt(i) == 'i' || texto.charAt(i) == 'o' || texto.charAt(i) == 'u') {
+                contadorVocales++;
+            }
+        }
+
+        return contadorVocales;
+    }
+
+
+    public static int contarPalabras(String texto) {
+
+        if (texto == null || texto.length() == 0) {
+            return 0;
+        }
+
+        int conteoPalabras = 0;
+
+        boolean dentroPalabra = false;
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caracterActual = texto.charAt(i);
+
+            if (caracterActual != ' ' && !dentroPalabra) {
+                conteoPalabras++;
+                dentroPalabra = true;
+            } else if (caracterActual == ' ') {
+                dentroPalabra = false;
+            }
+        }
+
+        return conteoPalabras;
+    }
+
+
+    public static String palabraMasLarga(String texto) {
+
+        if (texto == null || texto.length() == 0) {
+            return "";
+        }
+
+        String palabraActual = "";
+        String palabraMasLarga = "";
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caracterActual = texto.charAt(i);
+
+            if (caracterActual != ' ') {
+                palabraActual += caracterActual;
+            } else {
+                if (palabraActual.length() > palabraMasLarga.length()) {
+                    palabraMasLarga = palabraActual;
+                }
+                palabraActual = ""; // solo aquí
+            }
+        }
+
+        // Comparar la última palabra (por si el texto no termina en espacio)
+        if (palabraActual.length() > palabraMasLarga.length()) {
+            palabraMasLarga = palabraActual;
+        }
+
+        return palabraMasLarga;
+    }
+
+}
---
+++ gestorTareas.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class gestorTareas {
+    static final int MAX_TAREAS = 15;
+    static String[] tareas = new String[MAX_TAREAS];
+    static Scanner leer = new Scanner(System.in);
+    static int contador = 0;
+    public static void iniciarGestorTareas(String[] args) {
+        int opcion = 0;
+        do {
+        opcion = opcionMenu();
+        if (opcion == 0) {
+            System.out.println();
+            System.out.println("Saliendo del programa...");
+            System.out.println();
+        }
+        else {
+            llamadasFunciones(opcion);
+        }
+        } while (opcion != 0);
+    }
+
+    static int opcionMenu() {
+        int opcion = 0;
+        System.out.println();
+        System.out.println("=== GESTOR DE TAREAS ===");
+        System.out.println("OPCIONES:");
+        System.out.println("  1. Añadir Tarea");
+        System.out.println("  2. Listar Tareas");
+        System.out.println("  3. Buscar Palabra");
+        System.out.println("  O. Volver");
+        System.out.println();
+        System.out.print("Introduce tu opción (0-3): ");
+        opcion = leer.nextInt();
+        return opcion;
+    }
+
+    static void llamadasFunciones(int opcion) {
+        switch (opcion) {
+            case 1:
+                añadirTarea();
+                break;
+            case 2:
+                listarTareas();
+                break;
+            case 3:
+                pedirPalabra();
+                break;
+            default:
+                System.out.println();
+                System.out.println("Introduce una opción válida.");
+                System.out.println();
+                opcionMenu();
+        }
+    }
+    
+    static void añadirTarea() {
+        boolean existe = false;
+        if (contador == 15) {
+            System.out.println();
+            System.out.println("No te queda espacio para crear tareas nuevas.");
+        }
+        else {
+            System.out.println();
+            System.out.println("=== AÑADIR TAREA ===");
+            System.out.print("Introduce Tarea: ");
+            String tarea = leer.next();
+            for (int i = 0; i < contador; i++) {
+                if (tarea.toLowerCase().equals(tareas[i])) {
+                    System.out.println();
+                    System.out.println("Esta tarea ya existe en la posición " + i);
+                    System.out.println();
+                    existe = true;
+                }
+            }
+            if (existe == false) {
+                tareas[contador] = tarea;
+                contador++;
+                System.out.println();
+                System.out.println("Tarea '" +  tarea + "'correctamente creada");
+            }
+        }
+    }
+
+    static void listarTareas() {
+        System.out.println();
+        System.out.println("=== TAREAS ===");
+        if (contador == 0) {
+            System.out.println("No hay tareas para mostrar.");
+        }
+        else{
+            for (int i = 0; i < contador; i++) {
+            System.out.println(i + ". " + tareas[i]);            
+        }
+        }
+        System.out.println();
+    }
+
+    static void pedirPalabra() {
+        String palabra;
+        System.out.println("=== BUSCAR POR PALABRA ===");
+        System.out.print("Introduce palabra: ");
+        palabra = leer.next();
+        buscarPorPalabra(palabra);
+    }
+
+    static void buscarPorPalabra(String palabra) {
+        int encontradas = 0;
+        System.out.println();
+        System.out.println("Palabras encontradas:");
+        for (int i = 0; i < contador; i++) {
+            if (tareas[i].toLowerCase().contains(palabra)) {
+                System.out.println(i + ". "+ tareas[i]);
+                encontradas++;
+            }
+        }
+        System.out.println();
+        if (encontradas == 0) {
+            System.out.println("No se ha encontrado nada similar a " + palabra);
+        }
+        else {
+            System.out.println("Se han encontrado " + encontradas + " palabras relacionadas a '" + palabra + "'");
+        }
+    }
+}
